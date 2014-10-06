a7cf608a00bce7e9b53a98300963b2cb3424243a
Martin R. Hristov <martin.r.hristov@sap.com>
2014-10-07 00:45:37 +0300

    challenge 1

---
+++ challenge 1/HW.rb
@@ -0,0 +1,19 @@
+def format_string(str, width)
+  no_spaces = str.gsub(/^\s\s*/, '').gsub(/\s\s*$/, '').gsub(/\s{2,}/, ' ')
+
+  upcase_word = no_spaces.upcase
+
+  if no_spaces.size < width
+
+    spaces = width - no_spaces.size
+      for i in 0..spaces-1
+        if i.odd?
+          upcase_word.insert 0, ' '
+        else 
+          upcase_word.insert upcase_word.size, ' '
+        end
+      end
+  end
+
+  puts upcase_word
+end
